d277c51c77e04ec24ce964b7b7d2778afa44003e
Nathaniel Bauernfeind <natebauernfeind@deephaven.io>
2021-05-24 17:16:52 -0600

    change cmake based on chat w/corey

--- src/example.cc
+++ src/example.cc
@@ -99,7 +99,7 @@ int main(int argc, char** argv) {
       return 1;
     }
     return 0;
-  } catch (std::exception &e) {
-    std::cerr << e.what() << std::endl;
+  } catch (...) {
+    std::cerr << "Unknown Error" << std::endl;
   }
 }
