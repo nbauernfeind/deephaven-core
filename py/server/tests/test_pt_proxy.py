fa06c0d708f99b92ca183237cc33ccb491042709
Nathaniel Bauernfeind <natebauernfeind@deephaven.io>
2022-08-24 08:44:02 -0600

    Fix ExecutionContext and PartitionTable bugs caught by enabling parallel updates

--- py/server/tests/test_pt_proxy.py
+++ py/server/tests/test_pt_proxy.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2016-2022 Deephaven Data Labs and Patent Pending
 #
 
+import jpy
 import unittest
 
 from deephaven import read_csv, empty_table, SortDirection, AsOfMatchRule, DHError, time_table, ugp
@@ -292,7 +293,14 @@ class PartitionedTableProxyTestCase(BaseTestCase):
             ),
         ]
 
+        _JExecutionContext = jpy.get_type("io.deephaven.engine.context.ExecutionContext")
+        context = _JExecutionContext.newBuilder() \
+                .captureCompilerContext()         \
+                .captureQueryLibrary()            \
+                .emptyQueryScope()                \
+                .build().open()
         agg_pt_proxy = self.pt_proxy.agg_by(aggs=aggs, by=["a"])
+        context.close()
         for gct, ct in zip(agg_pt_proxy.target.constituent_tables, self.pt_proxy.target.constituent_tables):
             self.assertLessEqual(gct.size, ct.size)
             self.assertEqual(len(gct.columns), 8)
